4f70ac626f55ec819f57bc797d2d075219381866
Rilinda Xhaqkaj <xhaqkaj.r@gmail.com>
2021-05-28 23:26:54 +0200

    Post-Product-Page

--- Front-end/my-app/src/App.js
+++ Front-end/my-app/src/App.js
@@ -2,11 +2,14 @@ import './App.css'
 import WelcomePage from './components/nav/Welcome'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Footer from './components/footer/Footer'
+import PostProduct from './components/postProduct/PostProduct'
+
+
 
 function App() {
   return <div className="App">
-    <WelcomePage/>
-    <Footer/>
+    <PostProduct/>
+    
   </div>
 }
 

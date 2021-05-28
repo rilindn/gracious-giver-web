dce524a4add60dd4c527ae7522f5bb9a6e67fe58
rilind nuha <rn48707@ubt-uni.net>
2021-05-28 23:23:57 +0200

    home-page-nf

--- Front-end/my-app/src/components/home/Home.jsx
+++ Front-end/my-app/src/components/home/Home.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import { Badge, Button, ButtonGroup, Card, Col, Container, Form, Image, Row, ToggleButton, ToggleButtonGroup } from 'react-bootstrap'
-import { Item } from 'semantic-ui-react'
+import { Pagination, ToggleButton, ToggleButtonGroup } from 'react-bootstrap'
 import prod1 from "../../images/prod1.png"
 import prod2 from "../../images/prod2.png"
 
@@ -13,51 +12,186 @@ const Home = () => {
                 <ToggleButton className="m-1" style={{width:"80px"}} value={2}>Free</ToggleButton>
                 <ToggleButton className="border-right m-1" style={{width:"80px"}} value={3}>Needed</ToggleButton>
             </ToggleButtonGroup>
-            <div className="d-flex justify-content-center">
-                <div className="d-flex rowProd justify-content-center" >
-                    <Col xs={12} md={4} sm={6} lg={4} className="prodCol" >
-                        <div className="itemIcon">
-                                <div className="cropDiv">
-                                <Image width="100%" src={prod1} rounded></Image>
-                            </div>
-                            <div className="caption itemText">
-                                <h5>
-                                    <span className="itemTypeFree">FREE</span>
-                                    xeon processor
+
+            <div className="productsALL">
+                <div className="rowProd" >
+                    <div className="prodCol">
+                        <div className="home-prod">
+                            <img src={prod1} width="230px" height="230px" alt=""/>
+                            <div class="itemText">
+                                <h5 className="prodTitle">
+                                    <span class="itemTypeFree">FREE
+                                    </span> 
+                                        Xeon processor
                                 </h5>
-                                    Melbourne,Victoria
+                                <p className="prodLocation">Balaclava, Victoria</p> 
                             </div>
                         </div>
-                    </Col>
-                    <Col xs={12} md={4} sm={6} lg={4} className="prodCol">
-                    <div className="itemIcon">
-                                <div className="cropDiv">
-                                <Image width="100%" src={prod1} rounded></Image>
-                            </div>
-                            <div className="caption itemText">
-                                <h5>
-                                    <span className="itemTypeFree">FREE</span>
-                                    xeon processor
+                    </div>
+                    <div className="prodCol" >
+                        <div className="home-prod">
+                            <img src={prod1} width="230px" height="230px"/>
+                            <div class="caption itemText">
+                                <h5 className="prodTitle">
+                                    <span class="itemTypeFree">FREE
+                                    </span> 
+                                    Xeon processor
                                 </h5>
-                                    Melbourne,Victoria
+                                <p className="prodLocation">Balaclava, Victoria</p> 
                             </div>
                         </div>
-                    </Col>
-                    <Col xs={12} md={4} sm={6} lg={4} className="prodCol">
-                    <div className="itemIcon">
-                                <div className="cropDiv">
-                                <Image width="100%" src={prod1} rounded></Image>
-                            </div>
-                            <div className="caption itemText">
-                                <h5>
-                                    <span className="itemTypeFree">FREE</span>
-                                    xeon processor
+                    </div>
+                    <div className="prodCol" >
+                        <div className="home-prod">
+                            <img src={prod2} width="230px" height="230px"/>
+                            <div class="caption itemText">
+                                <h5 className="prodTitle">
+                                    <span class="itemTypeFree">FREE
+                                    </span> 
+                                    Xeon processorrrrrr
                                 </h5>
-                                    Melbourne,Victoria
+                                <p className="prodLocation">Balaclava, Victoria</p> 
                             </div>
                         </div>
-                    </Col>
+                    </div>
+
+                    <div className="prodCol" >
+                        <div className="home-prod">
+                            <img src={prod1} width="230px" height="230px" alt=""/>
+                            <div class="itemText">
+                                <h5 className="prodTitle">
+                                    <span class="itemTypeFree">FREE
+                                    </span> 
+                                        Xeon processorrr
+                                </h5>
+                                <p className="prodLocation">Balaclava, Victoria</p> 
+                            </div>
+                        </div>
+                    </div>
+                    <div className="prodCol" >
+                        <div className="home-prod">
+                            <img src={prod1} width="230px" height="230px"/>
+                            <div class="caption itemText">
+                                <h5 className="prodTitle">
+                                    <span class="itemTypeFree">FREE
+                                    </span> 
+                                    Xeon processor
+                                </h5>
+                                <p className="prodLocation">Balaclava, Victoria</p> 
+                            </div>
+                        </div>
+                    </div>
+                    <div className="prodCol" >
+                        <div className="home-prod">
+                            <img src={prod2} width="230px" height="230px"/>
+                            <div class="caption itemText">
+                                <h5 className="prodTitle">
+                                    <span class="itemTypeFree">FREE
+                                    </span> 
+                                    Xeon processor
+                                </h5>
+                                <p className="prodLocation">Balaclava, Victoria</p> 
+                            </div>
+                        </div>
+                    </div>
+                    <div className="prodCol" >
+                        <div className="home-prod">
+                            <img src={prod2} width="230px" height="230px"/>
+                            <div class="caption itemText">
+                                <h5 className="prodTitle">
+                                    <span class="itemTypeFree">FREE
+                                    </span> 
+                                    Xeon processor
+                                </h5>
+                                <p className="prodLocation">Balaclava, Victoria</p> 
+                            </div>
+                        </div>
+                    </div>
+                    <div className="prodCol" >
+                        <div className="home-prod">
+                            <img src={prod2} width="230px" height="230px"/>
+                            <div class="caption itemText">
+                                <h5 className="prodTitle">
+                                    <span class="itemTypeFree">FREE
+                                    </span> 
+                                    Xeon processor
+                                </h5>
+                                <p className="prodLocation">Balaclava, Victoria</p> 
+                            </div>
+                        </div>
+                    </div>
+                    <div className="prodCol" >
+                        <div className="home-prod">
+                            <img src={prod2} width="230px" height="230px"/>
+                            <div class="caption itemText">
+                                <h5 className="prodTitle">
+                                    <span class="itemTypeFree">FREE
+                                    </span> 
+                                    Xeon processor
+                                </h5>
+                                <p className="prodLocation">Balaclava, Victoria</p> 
+                            </div>
+                        </div>
+                    </div>
+                    <div className="prodCol" >
+                        <div className="home-prod">
+                            <img src={prod2} width="230px" height="230px"/>
+                            <div class="caption itemText">
+                                <h5 className="prodTitle">
+                                    <span class="itemTypeFree">FREE
+                                    </span> 
+                                    Xeon processor
+                                </h5>
+                                <p className="prodLocation">Balaclava, Victoria</p> 
+                            </div>
+                        </div>
+                    </div>
+                    <div className="prodCol" >
+                        <div className="home-prod">
+                            <img src={prod2} width="230px" height="230px"/>
+                            <div class="caption itemText">
+                                <h5 className="prodTitle">
+                                    <span class="itemTypeFree">FREE
+                                    </span> 
+                                    Xeon processor
+                                </h5>
+                                <p className="prodLocation">Balaclava, Victoria</p> 
+                            </div>
+                        </div>
+                    </div>
+                    <div className="prodCol" >
+                        <div className="home-prod">
+                            <img src={prod2} width="230px" height="230px"/>
+                            <div class="caption itemText">
+                                <h5 className="prodTitle">
+                                    <span class="itemTypeFree">FREE
+                                    </span> 
+                                    Xeon processor
+                                </h5>
+                                <p className="prodLocation">Balaclava, Victoria</p> 
+                            </div>
+                        </div>
+                    </div>
                 </div>
+            </div>
+            <div className="d-flex justify-content-center">
+                <Pagination>
+                    <Pagination.First />
+                    <Pagination.Prev />
+                    <Pagination.Item>{1}</Pagination.Item>
+                    <Pagination.Ellipsis />
+
+                    <Pagination.Item>{10}</Pagination.Item>
+                    <Pagination.Item>{11}</Pagination.Item>
+                    <Pagination.Item active>{12}</Pagination.Item>
+                    <Pagination.Item>{13}</Pagination.Item>
+                    <Pagination.Item >{14}</Pagination.Item>
+
+                    <Pagination.Ellipsis />
+                    <Pagination.Item>{20}</Pagination.Item>
+                    <Pagination.Next />
+                    <Pagination.Last />
+                </Pagination>
             </div>
         </div>
     )

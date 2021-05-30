1ffe9c6384a231bbb5a37373fa30736e9bba617e
Leonita Fetahu <leonitafetahu27@gmail.com>
2021-05-31 01:18:26 +0200

    sidebar-changes

--- Front-end/my-app/src/components/home/Home.jsx
+++ Front-end/my-app/src/components/home/Home.jsx
@@ -41,14 +41,14 @@ const Home = (props) => {
     return (
         <div>
             <Header/>
-            <Router>
+           
         <Sidebar />
         <Switch>
           <Route path=""  />
           <Route path="" />
           <Route path="" />
         </Switch>
-      </Router>
+     
             <div className="pt-5">
             <h3>Give away or find FREE second hand stuff</h3>
             <ToggleButtonGroup type="checkbox" defaultValue={[1, 3]} className="mb-3">
--- Front-end/my-app/src/components/postProduct/PostProduct.jsx
+++ Front-end/my-app/src/components/postProduct/PostProduct.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import Header from './../Header/Header';
+import Sidebar from './../Sidebar/Sidebar';
 import { Footer } from './../footer/Footer';
 import PostProdForm from './PostProdForm'
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 
 export const PostProduct = () => {
     return (
      <div>
         <Header/>
+        <Sidebar />
+        <Switch>
+          <Route path=""  />
+          <Route path="" />
+          <Route path="" />
+        </Switch>
         <div className="pt-3 prod-form-wrapper mx-auto">
             <h3  style ={{fontFamily:'Hanalei Fill'}} className="new-post-t">New Post</h3>    
             <div className ="txt-post-product mx-auto ">

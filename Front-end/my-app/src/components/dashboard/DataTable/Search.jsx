b9ab0e419f5b56ecdf34c5d913c65c64d4f623ab
rilind nuha <rn48707@ubt-uni.net>
2021-06-05 14:11:35 +0200

    table-updates

--- Front-end/my-app/src/components/dashboard/DataTable/Search.jsx
+++ Front-end/my-app/src/components/dashboard/DataTable/Search.jsx
@@ -15,7 +15,6 @@ export const Search = ({onSearch}) => {
             <input 
             type="text" 
             className="form-control"
-            style={{width:"240px"}}
             placeholder="Search"
             value={search}
             onChange={(e)=>onInputChange(e.target.value)}

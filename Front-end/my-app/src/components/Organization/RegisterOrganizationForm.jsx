d25f4b46c1a29615f0eef92fedb7e93c5b8431c0
Rilinda Xhaqkaj <xhaqkaj.r@gmail.com>
2021-07-03 00:50:44 +0200

    register-organizations-validation

--- Front-end/my-app/src/components/Organization/RegisterOrganizationForm.jsx
+++ Front-end/my-app/src/components/Organization/RegisterOrganizationForm.jsx
@@ -6,17 +6,34 @@ import { useHistory } from 'react-router-dom'
 import { NotificationManager } from 'react-notifications'
 import Footer from '../footer/Footer';
 import Header from '../Header/HeaderLoginRegister';
+import ValidationOrganization from './ValidationOrganization';
 
 const RegisterOrganizationForm = ({loggedInUser}) => {
 
     let history = useHistory()
     const [categories, setCategories] = useState([]);
+    const [errors, setErrors] = useState({});
     const [photoName, setPhotoName] = useState();
     const [fileName, setFileName] = useState();
     const [states, setStates] = useState([]);
     const [cities, setCities] = useState([]);
     const fileInput = useRef(null);
     const logoInput = useRef(null);
+
+    const [values,setValues] = useState({
+      username:"",
+      password:"",
+      name: "",
+      
+      email: "",
+    })
+
+    const handleChange = (event) => {
+      setValues({
+        ...values,
+        [event.target.name]:event.target.value,
+      })
+    }
 
     const handleFileInputClick = event => {
       fileInput.current.click();
@@ -99,6 +116,8 @@ const RegisterOrganizationForm = ({loggedInUser}) => {
 
     const handleSubmit = (event) => {
         event.preventDefault();
+        setErrors(ValidationOrganization(values));
+        if(Object.keys(errors).length===0){
         axios.post('http://localhost:5000/api/pendingorganizationsrequest', {
             Username: event.target.username.value,
             Password: event.target.password.value,
@@ -130,11 +149,11 @@ const RegisterOrganizationForm = ({loggedInUser}) => {
             },
           )
     }
-
+  }
     return (
         <div>
               <Header/>
-            <div className="pt-3 prod-form-wrapper mx-auto">
+            <div className="pt-3 prod-form-wrapper2 mx-auto">
             <h2  style ={{fontFamily:'Hanalei Fill'}} className="new-post-t1">Register as Organization</h2>
             <h6 className="new-post-j1">As an organization you can create events and initiatives to help people!</h6> 
         <Form 
@@ -143,39 +162,81 @@ const RegisterOrganizationForm = ({loggedInUser}) => {
         className ="form-components-width mx-auto">
           <Form.Group className="form-group-el d-flex">
             <Form.Label htmlFor="inputName">Username</Form.Label>
+            <div>
             <Form.Control
               style={{width: "400px"}}
               type="text"
               name="username"
+              required
+              value={values.username}
+              onChange={handleChange}
             />
+              <p className="error">{errors.username}</p>
+                    {errors.username &&
+                    <ul>
+                      <li className="info">{errors.usernameNote1}</li>
+                      <li className="info">{errors.usernameNote2}</li>
+                    </ul>}
+              </div>
+            
           </Form.Group>
 
           <Form.Group className="form-group-el d-flex">
             <Form.Label htmlFor="inputName">Password </Form.Label>
+            <div>
             <Form.Control
               style={{width: "400px"}}
               type="password"
               name="password"
+              required
+              value={values.password}
+              onChange={handleChange}
             />
+               <p className="error">{errors.password}</p>
+                   {errors.password &&
+                    <ul>
+                      <li className="info">{errors.passwordNote1}</li>
+                      <li className="info">{errors.passwordNote2}</li>
+                      <li className="info">{errors.passwordNote3}</li>
+                      <li className="info">{errors.passwordNote4}</li>
+                    </ul>}
+             </div>
           </Form.Group>
 
           
           <Form.Group className="form-group-el d-flex">
             <Form.Label htmlFor="inputName">Name </Form.Label>
+            <div>
             <Form.Control
               style={{width: "400px"}}
               type="text"
               name="name"
+              required
+              value={values.name}
+              onChange={handleChange}
             />
+             <p className="error">{errors.name}</p>
+                    {errors.name &&
+                    <ul>
+                      <li className="info">{errors.nameNote1}</li>
+                      <li className="info">{errors.nameNote2}</li>
+                    </ul>}
+              </div>
           </Form.Group>
 
           <Form.Group className="form-group-el d-flex">
             <Form.Label htmlFor="inputName">Email </Form.Label>
+            <div>
             <Form.Control
               style={{width: "400px"}}
               type="email"
               name="email"
+              required
+              value={values.email}
+              onChange={handleChange}
             />
+            <p className="error">{errors.email}</p>
+            </div>
           </Form.Group>
 
           <Form.Group className="form-group-el d-flex justify-content-start">
@@ -186,6 +247,7 @@ const RegisterOrganizationForm = ({loggedInUser}) => {
             type="button"
             onClick={handleLogoInputClick}
             className="orgUploadBtn"
+            
             >
               {photoName?photoName:"Choose file"}
             </button>
@@ -195,6 +257,7 @@ const RegisterOrganizationForm = ({loggedInUser}) => {
               name="logo"
               onChange={handlePhotoSubmit}
               ref={logoInput}
+              required
             />
             
           </Form.Group>
@@ -205,6 +268,7 @@ const RegisterOrganizationForm = ({loggedInUser}) => {
             type="button"
             onClick={handleFileInputClick}
             className="orgUploadBtn"
+           
             >
               {fileName?fileName:"Choose file"}
             </button>
@@ -214,6 +278,7 @@ const RegisterOrganizationForm = ({loggedInUser}) => {
               name="documentation"
               onChange={handleFileSubmit}
               ref={fileInput}
+              required
             />
           </Form.Group>
 
@@ -224,6 +289,7 @@ const RegisterOrganizationForm = ({loggedInUser}) => {
              style={{width: "400px"}} 
              as="select" 
              custom
+             required
              >
               {categories.map(categorie=>(
               <option>{categorie.OrganizationCategoryName}</option>
@@ -232,13 +298,19 @@ const RegisterOrganizationForm = ({loggedInUser}) => {
           </Form.Group> 
             <Form.Group className="form-group-el" controlId="exampleForm.ControlTextarea1">
             <Form.Label  >Description</Form.Label>
+            <div>
             <Form.Control 
             name="description" 
             placeholder ="Type here..." 
             style={{width: "400px"}} 
             as="textarea" 
             rows={3} 
+            required
+            value={values.description}
+            onChange={handleChange}
             />
+            <p className="error">{errors.description}</p>
+            </div>
           </Form.Group>
 
           <Form.Group className="form-group-el" controlId="exampleForm.SelectCustom">
@@ -250,7 +322,6 @@ const RegisterOrganizationForm = ({loggedInUser}) => {
              custom
              required
             >
-             <option></option>
              {states.map(state=>(
              <option value={state.Emri}>{state.Emri}</option>
             ))}
@@ -265,7 +336,6 @@ const RegisterOrganizationForm = ({loggedInUser}) => {
              custom
              required
             >
-             <option></option>
             {cities.map(city=>(
              <option value={city.CityName}>{city.CityName}</option>
              ))}
---
+++ Front-end/my-app/src/components/Organization/ValidationOrganization.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const ValidationOrganization = (values) => {
+
+    let errors ={};
+
+    if(!/[a-zA-Z0-9]{5,50}$/.test(values.username)){
+        errors.username = "*Username is not valid"
+        errors.usernameNote1 = "Username should be between 5 and 50 chars"
+        errors.usernameNote2 = "Only letters and numbers allowed"
+        return errors;
+    }
+    if(!/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/.test(values.password)){
+        errors.password = "* Invalid password!"
+        errors.passwordNote1 = "Minimum 8 characters"
+        errors.passwordNote2 = "At least one uppercase letter"
+        errors.passwordNote3 = "At least one lowercase letter"
+        errors.passwordNote4 = "At least one number"
+        return errors;
+    }
+    if(!/[a-zA-Z]{5,50}$/.test(values.name)){
+        errors.name = "*Name is not valid"
+        errors.nameNote1 = "Name should be between 5 and 50 chars"
+        errors.nameNote2 = "Only letters allowed"
+        return errors;
+    }
+    if(!/^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/.test(values.email)){
+        errors.email = "* Invalid email!"
+        return errors;
+    }
+    if(!/[A-Za-z0-9]{20,250}$/.test(values.description)){
+        errors.description = "*Minimum 20 characters!"
+        return errors;
+    }
+
+    return errors;
+}
+
+export default ValidationOrganization
